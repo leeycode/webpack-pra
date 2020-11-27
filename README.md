f5ab8fe929a5f5c13bab2741bc69e16b66c258bd
leeycode <349623647@qq.com>
2020-11-27 10:02:21 +0800

    add:npm script

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # webpack-pra
 webpack练习
+
+```
+# 默认安装
+npm init -y
+
+# 安装webpack相关
+npm install webpack webpack-cli --save-dev
+
+# 检查webpack版本 cd node_modules/.bin
+webpack -v
+
+# 通过npm stript运行webpack,否则只能在.bin下面执行
+
+```
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+'use strick';
+const path = require('path')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    mode: 'production'
+}
